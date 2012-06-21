5c8a89a334fbd6aa98a11e25b83c18ad50855669
Eugene Kalenkovich <rubify@softover.com>
2012-06-20 21:48:57 -0700

    Add meat to main routes : / and /done

--- app.rb
+++ app.rb
@@ -14,8 +14,20 @@ class App < Sinatra::Base
     DataMapper.finalize
     DataMapper.auto_upgrade!
   end
-
+  
+  before do
+    @s = Student.get(1) 
+  end
+  
   get '/' do
+    return "Welcome" unless @s
+    @s.new_workout if @s.on_plate.nil?
+    "#{@s.on_plate} #{@s.progress}"
+  end
+
+  get '/done' do
+    @s.advance
+    "#{@s.on_plate} #{@s.progress}"
   end
 
   run! if __FILE__ == $0
